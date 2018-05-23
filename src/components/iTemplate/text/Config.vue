c448e553246006dc2fd1cc556414941f5005a746
changj <2277068752@qq.com>
2018-05-23 17:18:29 +0800

    切换编辑器：由webstorm切换到vscode

---
+++ src/components/iTemplate/text/Config.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="MyContain">
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  },
+  created () {
+  },
+  mounted () {
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+.MyContain {
+  background-color: #fff;
+}
+</style>
